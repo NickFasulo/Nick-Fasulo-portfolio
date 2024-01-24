f845875a7e87b7a885161b588a0f52296041fd53
Nicholas Fasulo <fasulo.nick@gmail.com>
2024-01-24 12:26:18 -0500

    change project descriptions

--- data/config.js
+++ data/config.js
@@ -17,7 +17,7 @@ export const projects = {
       title: 'Top Stuff',
       image: '/static/top-stuff.png',
       description:
-        'An application that allows you to browse trending movies and view movie details. Includes infinite scrolling and the ability to see an AI generated review using OpenAI.',
+        'Trying to pick a movie? Top Stuff is an app that allows you to browse trending movies and view movie details. Includes infinite scrolling and the ability to see an AI generated review using OpenAI.',
       link: 'https://top-stuff.vercel.app',
       github: 'https://github.com/NickFasulo/top-stuff'
     },
@@ -25,7 +25,7 @@ export const projects = {
       title: 'Flippy Matchy',
       image: '/static/flippy-matchy.png',
       description:
-        'A match card game - to win you need to match each pair of identical cards in a deck. There are three levels to choose from: Easy, Medium, and Hard. The difference in levels are the amount of cards that are on the board. Unlock new categories to discover new card decks!',
+        'Bored at work? Play this match card game - to win you need to match each pair of identical cards in a deck. There are three levels to choose from: Easy, Medium, and Hard. Unlock new categories to discover new card decks!',
       link: 'https://nickfasulo.github.io/flippy-matchy-game',
       github: 'https://github.com/NickFasulo/flippy-matchy-game'
     },
@@ -33,7 +33,7 @@ export const projects = {
       title: 'Alike',
       image: '/static/alike.png',
       description:
-        'A social media platform where innovative minds collaborate and share ideas to create the projects of tomorrow. The platform is designed so developers can showcase their projects, share their feedback, and keep up on all the latest technology.',
+        'Looking to connect? Alike is a social media platform where innovative minds collaborate and share ideas to create the projects of tomorrow. The platform is designed so developers can showcase their projects, share their feedback, and keep up on all the latest technology.',
       link: 'https://alike-ga.netlify.app',
       github: 'https://github.com/NickFasulo/BackEnd-Alike'
     }
